5a2bdf69ba9aa5af465df87236f572ac5ebb69b7
AliZegler <ali12xx66@gmail.com>
2024-09-17 14:22:13 +0300

    remap chnaged leader q and w to leader q! and w!

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>q", vim.cmd.q)
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>q", ":q!<CR>")
+vim.keymap.set("n", "<leader>w", ":w!<CR>")
 
-vim.keymap.set("i", ";j", "<Esc>", { silent = true, desc = "which_key_ignore" })
+--vim.keymap.set("i", ";j", "<Esc>", { silent = true, desc = "which_key_ignore" })
 --vim.keymap.set({ "o", "v", "n", "x" }, ";j", "<Esc>", { silent = true, desc = "Normal Mode" })
 --vim.keymap.set({ "c" }, ";j", "<C-c>", { silent = true, desc = "which_key_ignore" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
