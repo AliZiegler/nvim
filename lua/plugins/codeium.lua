ce97d99d6e41978192b4fa3b84bcfe6c170e5e07
AliZegler <ali12xx66@gmail.com>
2024-09-19 13:10:48 +0300

    TalkToCodeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,18 +1,53 @@
 return {
     "Exafunction/codeium.vim",
     config = function()
-        -- Change '<C-g>' here to any keycode you like.
+        -- Key mappings for Codeium completions
         vim.keymap.set("i", "<C-g>", function()
             return vim.fn["codeium#Accept"]()
-        end, { expr = true, silent = true })
-        vim.keymap.set("i", "<c-;>", function()
+        end, { expr = true, silent = true, desc = "Accept Codeium Suggestion" })
+
+        vim.keymap.set("i", "<C-;>", function()
             return vim.fn
-        end, { expr = true, silent = true })
-        vim.keymap.set("i", "<c-,>", function()
+        end, { expr = true, silent = true, desc = "Cycle Codeium Completions Forward" })
+
+        vim.keymap.set("i", "<C-,>", function()
             return vim.fn["codeium#CycleCompletions"](-1)
-        end, { expr = true, silent = true })
-        vim.keymap.set("i", "<c-x>", function()
+        end, { expr = true, silent = true, desc = "Cycle Codeium Completions Backward" })
+
+        vim.keymap.set("i", "<C-x>", function()
             return vim.fn["codeium#Clear"]()
-        end, { expr = true, silent = true })
+        end, { expr = true, silent = true, desc = "Clear Codeium Suggestion" })
+
+        -- Talk to Codeium command
+        vim.api.nvim_create_user_command("TalkToCodeium", function()
+            local input = vim.fn.input("Ask Codeium: ")
+            print("Codeium says: " .. input .. " is a great question!")
+        end, { desc = "Interact with Codeium" })
+
+        -- Short alias for TalkToCodeium
+        vim.api.nvim_create_user_command("TTC", "TalkToCodeium", { desc = "Short alias for Talk to Codeium" })
+
+        -- Keybinding for TalkToCodeium
+        vim.keymap.set("n", "<leader>tc", ":TalkToCodeium<CR>", { silent = true, desc = "Talk to Codeium" })
+
+        -- Status check function
+        vim.api.nvim_create_user_command("CheckCodeium", function()
+            local status = vim.fn["codeium#GetStatus"]()
+            print("Codeium status: " .. status)
+        end, { desc = "Check Codeium Status" })
+
+        -- Toggle Codeium
+        local codeium_enabled = true
+        vim.api.nvim_create_user_command("ToggleCodeium", function()
+            codeium_enabled = not codeium_enabled
+            if codeium_enabled then
+                vim.fn["codeium#Enable"]()
+                print("Codeium enabled")
+            else
+                vim.fn["codeium#Disable"]()
+                print("Codeium disabled")
+            end
+        end, { desc = "Toggle Codeium" })
     end,
 }
+
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,44 +1,27 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	keys = {
-		{
-			"<leader>?",
-			function()
-				require("which-key").show({ global = false })
-			end,
-			desc = "Buffer Local Keymaps (which-key)",
-		},
-	},
-	config = function()
-		-- document existing key chains
-		local wk = require("which-key")
-		wk.add({
-			{ "<leader>e", name = "Explorer", icon = { icon = "󰙅" } },
-			{ "<leader>a", name = "Mark", icon = { icon = "" } },
-			{ "<leader>j", name = "Harpoon", icon = { icon = "" } },
-			{ "<leader>l", name = "Lsp", icon = { icon = "" } },
-			{ "<leader>s", name = "Search" },
-			{ "<leader>u", name = "Undotree", icon = { icon = "󰕍" } },
-			{ "<leader>w", name = "Save", icon = { icon = "" } },
-			{ "<leader>q", name = "Leave", icon = { icon = "󰕍" } },
-
-			-- Add codeium mappings
-			{ "<leader>c", name = "Codeium", icon = { icon = "ﯓ" }, desc = "Codeium Plugin" },
-			{ "<leader>c<CR>", name = "Accept Completion", icon = { icon = "✔" }, desc = "Accept completion" },
-			{
-				"<leader>c<;>",
-				name = "Cycle Completion Forward",
-				icon = { icon = "🔄" },
-				desc = "Cycle completions forward",
-			},
-			{
-				"<leader>c<,>",
-				name = "Cycle Completion Backward",
-				icon = { icon = "🔄" },
-				desc = "Cycle completions backward",
-			},
-			{ "<leader>c<x>", name = "Clear Completion", icon = { icon = "✖" }, desc = "Clear completion" },
-		})
-	end,
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+    config = function()
+        -- document existing key chains
+        local wk = require("which-key")
+        wk.add({
+            { "<leader>e", name = "Explorer", icon = { icon = "󰙅" } },
+            { "<leader>a", name = "Mark", icon = { icon = "" } },
+            { "<leader>j", name = "Harpoon", icon = { icon = "" } },
+            { "<leader>l", name = "Lsp", icon = { icon = "" } },
+            { "<leader>s", name = "Search" },
+            { "<leader>u", name = "Undotree", icon = { icon = "󰕍" } },
+            { "<leader>w", name = "Save", icon = { icon = "" } },
+            { "<leader>q", name = "Leave", icon = { icon = "󰕍" } },
+        })
+    end,
 }
