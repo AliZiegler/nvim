7a587273b139591fdd025017bb66ae50a6e3debd
AliZegler <ali12xx66@gmail.com>
2024-09-19 11:52:10 +0300

    added codeium for some help

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -22,6 +22,23 @@ return {
 			{ "<leader>u", name = "Undotree", icon = { icon = "󰕍" } },
 			{ "<leader>w", name = "Save", icon = { icon = "" } },
 			{ "<leader>q", name = "Leave", icon = { icon = "󰕍" } },
+
+			-- Add codeium mappings
+			{ "<leader>c", name = "Codeium", icon = { icon = "ﯓ" }, desc = "Codeium Plugin" },
+			{ "<leader>c<CR>", name = "Accept Completion", icon = { icon = "✔" }, desc = "Accept completion" },
+			{
+				"<leader>c<;>",
+				name = "Cycle Completion Forward",
+				icon = { icon = "🔄" },
+				desc = "Cycle completions forward",
+			},
+			{
+				"<leader>c<,>",
+				name = "Cycle Completion Backward",
+				icon = { icon = "🔄" },
+				desc = "Cycle completions backward",
+			},
+			{ "<leader>c<x>", name = "Clear Completion", icon = { icon = "✖" }, desc = "Clear completion" },
 		})
 	end,
 }
