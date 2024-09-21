9e2f887f351adb9725643827976684bdc10654b7
AliZegler <ali12xx66@gmail.com>
2024-09-21 14:44:37 +0300

    telescope-themes theme changer <leader>th

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,21 +1,21 @@
 --return { "rose-pine/neovim", name = "rose-pine" , config= function()
 --	require("rose-pine").setup({
 --    styles = {
- --       transparency = true,
-  --  }})
+--       transparency = true,
+--  }})
 --vim.cmd.colorscheme 'rose-pine'
 --end}
 return {
-  -- Theme inspired by Atom
-  'navarasu/onedark.nvim',
-  priority = 1000,
-  config = function()
-    require('onedark').setup {
-      transparent = true,
-      style = 'warmer',
-    }
-    require('onedark').load()
-    vim.cmd.colorscheme 'onedark'
-  end,
+    -- Theme inspired by Atom
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+        require("onedark").setup({
+            transparent = true,
+            style = "warmer",
+        })
+        require("onedark").load()
+        vim.cmd.colorscheme("onedark")
+    end,
 }
 -- return { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 }
