05f1f66a7b3a16234bd76b99bcb198513ef58fb7
AliZegler <ali12xx66@gmail.com>
2024-09-22 12:42:45 +0300

    css-color, smoothscroll and vim-surround and their keymaps

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -6,8 +6,8 @@ vim.keymap.set("n", "<leader>w", ":w!<CR>")
 --vim.keymap.set("i", ";j", "<Esc>", { silent = true, desc = "which_key_ignore" })
 --vim.keymap.set({ "o", "v", "n", "x" }, ";j", "<Esc>", { silent = true, desc = "Normal Mode" })
 --vim.keymap.set({ "c" }, ";j", "<C-c>", { silent = true, desc = "which_key_ignore" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
+--vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
+--vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
 -- Make yank use system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to clipboard" })
@@ -18,3 +18,35 @@ vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste from clipboar
 vim.keymap.set("n", "<leader>th", ":Telescope themes<CR>", { noremap = true, silent = true, desc = "Theme Switcher" })
 --Twilight
 vim.keymap.set("n", "<leader>tw", ":Twilight<CR>", { noremap = true, silent = true, desc = "Twilight" })
+--vim-surround
+vim.keymap.set("n", "ds", "ds", { noremap = true, silent = true, desc = "Delete Surround" })
+vim.keymap.set("n", "cs", "cs", { noremap = true, silent = true, desc = "Change Surround" })
+vim.keymap.set("n", "ys", "ys", { noremap = true, silent = true, desc = "Add Surround" })
+vim.keymap.set("n", "ysiw", "ysiw", { noremap = true, silent = true, desc = "Surround Inner Word" })
+vim.keymap.set("n", "yss", "yss", { noremap = true, silent = true, desc = "Wrap an entire line" })
+vim.keymap.set("v", "S", "S", { noremap = true, silent = true, desc = "Surround Visual Selection" })
+--vim-smooth-scroll
+vim.keymap.set(
+	"n",
+	"<C-u>",
+	":call smooth_scroll#up(&scroll, 0, 2)<CR>zz",
+	{ noremap = true, silent = true, desc = "Smooth move up half-page" }
+)
+vim.keymap.set(
+	"n",
+	"<C-d>",
+	":call smooth_scroll#down(&scroll, 0, 2)<CR>zz",
+	{ noremap = true, silent = true, desc = "Smooth move down half-page" }
+)
+vim.keymap.set(
+	"n",
+	"<C-b>",
+	":call smooth_scroll#up(&scroll*2, 0, 4)<CR>zz",
+	{ noremap = true, silent = true, desc = "Smooth move up page" }
+)
+vim.keymap.set(
+	"n",
+	"<C-f>",
+	":call smooth_scroll#down(&scroll*2, 0, 4)<CR>zz",
+	{ noremap = true, silent = true, desc = "Smooth move down page" }
+)
