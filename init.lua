7b31529fb850f54408a5ca01e5dfbe601d90c141
AliZegler <ali12xx66@gmail.com>
2024-09-24 13:18:32 +0300

    lsp.lua correct insure install lspserver names

--- init.lua
+++ init.lua
@@ -9,3 +9,11 @@ vim.opt.clipboard = "unnamedplus"
 -- Set leaders before lazy.nvim to ensure mappings are correct
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+-- file colors
+-- Custom highlight for JavaScript files (Green)
+-- vim.cmd([[autocmd FileType javascript highlight CustomJS guifg=#98C379]])
+-- vim.cmd([[autocmd FileType javascript syntax match CustomJS /\v.*/ | highlight link CustomJS Statement]])
+--
+-- -- Custom highlight for TypeScript files (Blue)
+-- vim.cmd([[autocmd FileType typescript highlight CustomTS guifg=#61AFEF]])
+-- vim.cmd([[autocmd FileType typescript syntax match CustomTS /\v.*/ | highlight link CustomTS Statement]])
