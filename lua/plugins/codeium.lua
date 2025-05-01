481fac8971c8575876ab839d02b10b8018a45fa1
AliZegler <ali12xx66@gmail.com>
2025-05-01 10:59:55 +0300

    codeium and null changes idk tbh

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,45 +1,66 @@
 return {
     "Exafunction/codeium.vim",
+    event = { "BufReadPre", "BufNewFile" }, -- Load only when needed
+    dependencies = {
+        "nvim-lua/plenary.nvim",         -- Optional: for better async handling
+    },
     config = function()
-        -- Key mappings for Codeium completions
-        vim.keymap.set("i", "<C-f>", function()
-            return vim.fn["codeium#Accept"]()
-        end, { expr = true, silent = true, desc = "Accept Codeium Suggestion" })
+        -- Configuration options
+        vim.g.codeium_disable_bindings = 1 -- Disable default bindings
+        vim.g.codeium_idle_delay = 75 -- Reduce delay for suggestions (default: 75ms)
+        vim.g.codeium_manual_trigger = true -- Enable manual trigger mode
 
-        vim.keymap.set("i", "<C-o>", function()
-            return vim.fn
-        end, { expr = true, silent = true, desc = "Cycle Codeium Completions Forward" })
-
-        vim.keymap.set("i", "<C-,>", function()
-            return vim.fn["codeium#CycleCompletions"](-1)
-        end, { expr = true, silent = true, desc = "Cycle Codeium Completions Backward" })
-
-        vim.keymap.set("i", "<C-x>", function()
-            return vim.fn["codeium#Clear"]()
-        end, { expr = true, silent = true, desc = "Clear Codeium Suggestion" })
-
-        -- Talk to Codeium command
-        vim.api.nvim_create_user_command("TalkToCodeium", function()
-            local input = vim.fn.input("Ask Codeium: ")
-            print("Codeium says: " .. input .. " is a great question!")
-        end, { desc = "Interact with Codeium" })
-        -- Status check function
-        vim.api.nvim_create_user_command("CheckCodeium", function()
-            local status = vim.fn["codeium#GetStatus"]()
-            print("Codeium status: " .. status)
-        end, { desc = "Check Codeium Status" })
-
-        -- Toggle Codeium
-        local codeium_enabled = true
-        vim.api.nvim_create_user_command("ToggleCodeium", function()
-            codeium_enabled = not codeium_enabled
-            if codeium_enabled then
-                vim.fn["codeium#Enable"]()
-                print("Codeium enabled")
-            else
-                vim.fn["codeium#Disable"]()
-                print("Codeium disabled")
+        -- Key mappings with better descriptions
+        local function map(mode, lhs, rhs, opts)
+            local options = { silent = true }
+            if opts then
+                options = vim.tbl_extend("force", options, opts)
             end
-        end, { desc = "Toggle Codeium" })
+            vim.keymap.set(mode, lhs, rhs, options)
+        end
+
+        -- Completion keybindings
+        map("i", "<C-f>", function()
+            return vim.fn["codeium#Accept"]()
+        end, { expr = true, desc = "Accept Codeium suggestion" })
+        map("i", "<C-n>", function()
+            return vim.fn["codeium#CycleCompletions"](1)
+        end, { expr = true, desc = "Next Codeium suggestion" })
+        map("i", "<C-p>", function()
+            return vim.fn["codeium#CycleCompletions"](-1)
+        end, { expr = true, desc = "Previous Codeium suggestion" })
+        map("i", "<C-x>", function()
+            return vim.fn["codeium#Clear"]()
+        end, { expr = true, desc = "Clear Codeium suggestion" })
+
+        -- Enhanced status function with more details
+        local function get_detailed_status()
+            local status = vim.fn["codeium#GetStatusString"]()
+            local enabled = vim.g.codeium_enabled == 1 and "Enabled" or "Disabled"
+            return string.format("Codeium: %s | Status: %s", enabled, status)
+        end
+
+        -- Commands with better feedback
+        vim.api.nvim_create_user_command("CodeiumStatus", function()
+            vim.notify(get_detailed_status(), vim.log.levels.INFO)
+        end, { desc = "Show Codeium detailed status" })
+        -- Chat functionality with better UI feedback
+        vim.api.nvim_create_user_command("CodeiumChat", function()
+            -- Create a floating window for input
+            local input = vim.fn.input({
+                prompt = "Ask Codeium: ",
+                completion = "custom,CodeiumChatComplete",
+            })
+            if input ~= "" then
+                -- TODO: Integrate with Codeium's chat API when available
+                vim.notify("Chat feature coming soon!", vim.log.levels.INFO)
+            end
+        end, { desc = "Chat with Codeium" })
+
+        -- Statusline component
+        -- Can be used with lualine or other statusline plugins
+        _G.codeium_status = function()
+            return vim.fn["codeium#GetStatusString"]()
+        end
     end,
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,42 +1,42 @@
 return {
-	"nvimtools/none-ls.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"mason.nvim",
-		"jay-babu/mason-null-ls.nvim",
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		local formatting = null_ls.builtins.formatting
-		local diagnostics = null_ls.builtins.diagnostics
-		local code_actions = null_ls.builtins.code_actions
+    "nvimtools/none-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "mason.nvim",
+        "jay-babu/mason-null-ls.nvim",
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        local formatting = null_ls.builtins.formatting
+        local diagnostics = null_ls.builtins.diagnostics
+        local code_actions = null_ls.builtins.code_actions
 
-		-- Set up Mason for null-ls
-		require("mason-null-ls").setup({
-			ensure_installed = {
-				"prettier",
-				"stylua",
-				"selene",
-			},
-			automatic_installation = true,
-		})
+        -- Set up Mason for null-ls
+        require("mason-null-ls").setup({
+            ensure_installed = {
+                "prettier",
+                "stylua",
+                "selene",
+            },
+            automatic_installation = true,
+        })
 
-		-- Configure null-ls
-		null_ls.setup({
-			sources = {
+        -- Configure null-ls
+        null_ls.setup({
+            sources = {
 
-				-- Lua
-				formatting.stylua,
-				-- diagnostics.selene, -- Uncomment if you want to enable selene diagnostics
+                -- Lua
+                formatting.stylua,
+                -- diagnostics.selene, -- Uncomment if you want to enable selene diagnostics
 
-				-- Git and refactoring tools
-				code_actions.gitsigns,
-				code_actions.refactoring,
-			},
-			on_attach = function(client, bufnr)
-				-- Set up buffer-specific keymaps or settings if necessary
-			end,
-		})
-	end,
+                -- Git and refactoring tools
+                code_actions.gitsigns,
+                code_actions.refactoring,
+            },
+            on_attach = function(client, bufnr)
+                -- Set up buffer-specific keymaps or settings if necessary
+            end,
+        })
+    end,
 }
