e779cac145739674df432526169ae98c2908d655
AliZiegler <ali12xx66@gmail.com>
2025-06-12 11:32:19 +0300

    removed mason registery

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,6 @@ return {
         "neovim/nvim-lspconfig",
         {
             "williamboman/mason.nvim",
-            dependencies = { "mason-org/mason-registry" },
             config = function()
                 require("mason").setup()
             end,
